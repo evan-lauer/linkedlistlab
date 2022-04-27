717c612586f1165c30d63269a25ef6945257648b
debenedettig <debenedettig@mantis.mathcs.carleton.edu>
2022-04-27 15:19:25 -0500

    cons

--- linkedlist.c
+++ linkedlist.c
@@ -1,6 +1,6 @@
 #include <stdbool.h>
 
-typedef enum {INT_TYPE, DOUBLE_TYPE, STR_TYPE, NULL_TYPE} valueType;
+typedef enum {INT_TYPE, DOUBLE_TYPE, STR_TYPE, NULL_TYPE, CONS_TYPE} valueType;
 // grace is here
 struct Value {
     valueType type; // type will also have a CONS_TYPE as an option
